8e3efc4e7ce0d74bc67f421cc39202d8435c341c
saad <sy.2.hussain@gmail.com>
2013-08-28 16:38:10 +0530

    first commit

---
+++ branchTest/branchTest.cpp
@@ -0,0 +1,14 @@
+// branchTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char text[100];
+	cin>>text;
+	cout<<text;
+	return 0;
+}
+
